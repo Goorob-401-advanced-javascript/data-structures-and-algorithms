b15862e76a4ae607e062177d377444f3cb57dea6
Goorob <orobalswalqah@gmail.com>
2020-01-25 22:48:05 +0200

    adding .eslintrc.json

--- __test__/array-binary-search.test.js
+++ __test__/array-binary-search.test.js
@@ -1,5 +1,5 @@
 'use strict';
-const validate = require('../challanges/array-binary-search/array-binary-search');
+const validate = require('../challanges/array-binary-search/array-binary-search.js');
 describe('Validitor Module',()=>{
     it('validate if the element inside the array it will return the index of that element',()=>{
         expect(validate([1,2,3],3)).toEqual(2)
