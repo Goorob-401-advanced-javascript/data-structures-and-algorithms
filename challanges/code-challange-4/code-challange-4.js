7989359d2fa604ac5e31f0973d171d94fe129a50
LTUC Student 1 <std1@QUDSLABS.LOCAL>
2020-02-03 21:40:34 +0200

    code challange 4 completed

--- challanges/code-challange-4/code-challange-4.js
+++ challanges/code-challange-4/code-challange-4.js
@@ -3,9 +3,9 @@ function fibFunction(x){
   let array = [0, 1];
   for (let i = array.length; i < x + 1; i++){
     let sum = array[i - 2] + array[i -1];
-    array.push(sum)
+    array.push(sum);
   }
-  return array[x]
+  return array[x];
 }
 
 module.exports = fibFunction ;
--- challanges/code-challange-4/code-challange-4.test.js
+++ challanges/code-challange-4/code-challange-4.test.js
@@ -1,8 +1,7 @@
 'use strict';
-
-let validator = require('../code-challange4.js');
+let validator = require('./code-challange-4.js');
 describe('Fibonacci Module',()=>{
   it('validate  the output arr  ' ,()=> {
-    expect(validator(8)).toEqual( 21 )
+    expect(validator(8)).toEqual( 21 );
   });
-})
+}); 
