acd509793fb34abca476aeccf83469730f4151ce
LTUC Student 1 <std1@QUDSLABS.LOCAL>
2020-03-11 23:53:58 +0200

    fixing lint

--- challanges/repeatedWord/repeatedWord.js
+++ challanges/repeatedWord/repeatedWord.js
@@ -1,37 +1,37 @@
 
 'use strict';
 function findRepeatedWord(str){
-    let splitword = split(str);
+  let splitword = split(str);
   
-    for(let i=0; i < splitword.length; i++){
-      for(let j=0; j < splitword.length; j++){
-        if(splitword[i] === splitword[j] && i !== j){
-          return splitword[i];
-        }
+  for(let i=0; i < splitword.length; i++){
+    for(let j=0; j < splitword.length; j++){
+      if(splitword[i] === splitword[j] && i !== j){
+        return splitword[i];
       }
     }
-    return 'no match';
   }
+  return 'no match';
+}
   
-  function split(str) {
-    let word = [];
-    let wordPending = '';
-    for (let i=0; i < str.length; ++i) {
-      if (str[i] === ' ') { 
-        word.push(wordPending);
-        wordPending = ''; 
-      } else {
-        wordPending += str[i]; 
-      }
+function split(str) {
+  let word = [];
+  let wordPending = '';
+  for (let i=0; i < str.length; ++i) {
+    if (str[i] === ' ') { 
+      word.push(wordPending);
+      wordPending = ''; 
+    } else {
+      wordPending += str[i]; 
     }
+  }
   
   
     
-    if (wordPending.length > 0) {
-      word.push(wordPending); // 
-    }
-  
-    return word;
+  if (wordPending.length > 0) {
+    word.push(wordPending); // 
   }
   
-  module.exports = findRepeatedWord;
+  return word;
+}
+  
+module.exports = findRepeatedWord;
